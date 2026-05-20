82ecb15ecf40775fea2214bd492cc0c032cb30b3
Leo Z <jinluezhang@gmail.com>
2026-05-20 16:39:41 +0100

    Add new record page with form validation and POST to API

--- RecordShop/RecordShop/Model/Record.cs
+++ RecordShop/RecordShop/Model/Record.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Album
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string Artist { get; set; }
+    [Required]
     public string Genre { get; set; }
+    [Range(1900, 2100)]
     public int ReleaseYear { get; set; }
     public int Stock { get; set; }
+    [Range(0.01, 99999.99)]
     public decimal Price { get; set; }
 }
