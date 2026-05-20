9e141f1cb17104315922144ec96bf7b41b926e7e
Leo Z <jinluezhang@gmail.com>
2026-05-20 12:35:33 +0100

    Connect to API with RecordsLoader and update Album model

--- RecordShop/RecordShop/Model/Record.cs
+++ RecordShop/RecordShop/Model/Record.cs
@@ -1,7 +1,10 @@
-public class Record
+public class Album
 {
-    public string Title {get; set;}
-    public string Artist {get; set;}
-    public string Genre {get; set;}
-    public decimal Price {get; set;}
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Artist { get; set; }
+    public string Genre { get; set; }
+    public int ReleaseYear { get; set; }
+    public int Stock { get; set; }
+    public decimal Price { get; set; }
 }
