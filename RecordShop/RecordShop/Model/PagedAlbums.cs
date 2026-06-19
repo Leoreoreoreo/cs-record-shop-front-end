b9cb954cba45eb4a6f20c7d812ef8e7881ebb0d4
Leoreoreoreo <jinluezhang@gmail.com>
2026-06-19 12:59:51 +0100

    Add paging to the all records page

---
+++ RecordShop/RecordShop/Model/PagedAlbums.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+public class PagedAlbums
+{
+    public List<Album> Albums { get; set; } = new();
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages { get; set; }
+}
